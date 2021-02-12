36033932beb2e0539b6245c92082558fefa6b55c
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-02-12 20:32:59 +0600

    making child component of layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddRecordComponent } from './add-record/add-record.component';
-import { ResultsComponent } from './results/results.component';
-import { NotesComponent } from './notes/notes.component';
+import { AddRecordComponent } from './layout/add-record/add-record.component';
+import { ResultsComponent } from './layout/results/results.component';
+import { NotesComponent } from './layout/notes/notes.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddRecordComponent,
     ResultsComponent,
-    NotesComponent
+    NotesComponent,
+    HeaderComponent,
+    LayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
